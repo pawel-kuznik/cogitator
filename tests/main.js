563446911b6e2f5d81ff50487e8c6673655a6974
pawel <pawel.kuznik@gmail.com>
2019-07-31 22:21:12 +0200

    Updated documentation

--- tests/main.js
+++ tests/main.js
@@ -1,3 +1,9 @@
+/**
+ *  This is a kickstart file for the tests. We have it like this cause we want
+ *  to use our regular environment (commonjs modules compiled with browserify).
+ *  To achieve this we need to compile tests just like we would compile the
+ *  actual application.
+ */
 
-
+// require all tests
 require('./Modal.js');
