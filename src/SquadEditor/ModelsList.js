5e12048f41d3bfa35d4ef4b7dff7335feca05559
pawel <pawel.kuznik@gmail.com>
2019-10-29 21:55:58 +0100

    added documentation

--- src/SquadEditor/ModelsList.js
+++ src/SquadEditor/ModelsList.js
@@ -1,13 +1,15 @@
 /**
  *  This is a component that allows managing list of models associated
  *  with a squad template. 
+ *
+ *  @author     Paweł Kuźnik <pawel.kuznik@gmail.com>
+ *  @file       src/SquadEditor/ModelsList.js
  */
 
 // the dependencies
 const EditorList    = require('../common/EditorList.js');
 const ModelEditor   = require('../ModelTemplateEditor.js');
 const Item          = require('../common/EditorItem.js');
-
 
 // export the component
 module.exports = class extends EditorList {
